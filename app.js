24582149b73122ae07bee64078c02750a05a375c
gunturkh <guntur.kh@gmail.com>
2018-08-13 10:20:25 +0700

    Remove OPTION method in CORS header

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./api/index');
-// const usersRouter = require('./api/users');
 const todoRouter = require('./api/todo/');
 
 
@@ -32,7 +31,7 @@ app.use(function (req, res, next) {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
